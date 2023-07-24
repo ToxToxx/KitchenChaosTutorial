2b90a1c2447e193b2204f97c01fdaa5f4dfbbd5e
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2023-07-24 20:59:46 +0300

    add assets, create player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector2 inputVector = new Vector2(0, 0);
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+        }       
+        else if (Input.GetKey(KeyCode.D))
+        {
+        }
+
+    }
+}
