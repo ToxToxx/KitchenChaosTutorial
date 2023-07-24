d05710280d9dc07bcf42b493be5dfde8673d8592
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2023-07-25 01:47:17 +0300

    Add Animation and rotation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _moveSpeed = 7f;
+    private bool _isWalking = false;
 
     // Start is called before the first frame update
     void Start()
@@ -37,9 +39,17 @@ public class Player : MonoBehaviour
 
         inputVector = inputVector.normalized;
 
-        Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
+        Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y); //movement
         transform.position += moveDirection *  _moveSpeed * Time.deltaTime;
-        Debug.Log(Time.deltaTime);
+        _isWalking = moveDirection != Vector3.zero;
 
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed); //rotation and smoothing
+
+    }
+
+    public bool IsWalking()
+    {
+        return _isWalking;
     }
 }
