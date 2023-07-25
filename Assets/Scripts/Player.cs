e310296aecfed641983c55255c4050973a1e28a9
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2023-07-25 16:29:23 +0300

    Refactor Input System, Change Camera

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,7 +8,8 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 7f;
     private bool _isWalking = false;
-
+    [SerializeField] private GameInput _gameInput;
+ 
     // Start is called before the first frame update
     void Start()
     {
@@ -18,26 +19,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-        
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }       
-        else if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-
-        inputVector = inputVector.normalized;
+        Vector2 inputVector =  _gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y); //movement
         transform.position += moveDirection *  _moveSpeed * Time.deltaTime;
