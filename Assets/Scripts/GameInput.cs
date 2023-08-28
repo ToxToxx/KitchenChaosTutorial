a1d012036ee248a73ddc7225263a9e9ac99137f6
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2023-08-28 20:48:46 +0300

    Making tutorial window

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -13,6 +13,7 @@ public class GameInput : MonoBehaviour
     public event EventHandler OnInteractAction;//event on interaction
     public event EventHandler OnInteractAlternateAction;
     public event EventHandler OnPauseAction;
+    public event EventHandler OnBindingRebind;
 
     public enum Binding
     {
@@ -175,6 +176,8 @@ public class GameInput : MonoBehaviour
                 playerInputActions.SaveBindingOverridesAsJson();
                 PlayerPrefs.SetString(PLAYER_PREFS_BINDINGS, playerInputActions.SaveBindingOverridesAsJson());
                 PlayerPrefs.Save();
+
+                OnBindingRebind?.Invoke(this, EventArgs.Empty);  
             }).Start();
     }
 }
